d77fec393aaff50ba55c7913700b4365d829fc1b
ShinFujimoto <sfujimot1182@gmail.com>
2019-12-15 04:17:32 +0900

    minor changes

---
+++ AppSwoosh/ClassChoiceViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ClassChoiceViewController.swift
+//  AppSwoosh
+//
+//  Created by shin fujimoto on 2019/12/15.
+//  Copyright © 2019 shin fujimoto. All rights reserved.
+//
+
+import UIKit
+
+class ClassChoiceViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ AppSwoosh/LoginViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  AppSwoosh
+//
+//  Created by shin fujimoto on 2019/12/12.
+//  Copyright © 2019 shin fujimoto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var swoosh: UIImageView!
+    @IBOutlet weak var bgImageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+
+    }
+
+
+}
+
